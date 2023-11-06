aaf35d59d6bc5e53106474af5540cbdda5051b0e
Natassa Farmaki <natazf@gmail.com>
2023-11-06 18:43:27 +0100

    initial commit

---
+++ src/shingling.py
@@ -0,0 +1,16 @@
+
+class Shingling:
+
+    def __init__(self, k, hash_fun):
+        self.k = k
+        self.hash_fun = hash_fun
+
+    def create_shingles(document):
+        pass
+
+
+    def hash_document(self):
+        pass
+
+    def hash_shingle(self, shingle):
+        pass
